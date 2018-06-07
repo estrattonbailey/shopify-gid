ce0a710a9a585dbcce785c789283570beb700ce4
estrattonbailey <ericstrattonbailey@gmail.com>
2018-06-07 07:24:37 -0400

    Add support for params

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # shopify-gid
 Universal encoder/decoder for ID values returned from the Shopify Storefront
-GraphQL API. 221 bytes gzipped.
+GraphQL API. **400 bytes gzipped.**
 
 ## Install
 ```bash
@@ -10,12 +10,16 @@ npm i shopify-gid --save
 # Usage
 ```javascript
 import { encode, decode } from 'shopify-gid'
-
-encode('Product', 12345)
-  // => Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzEyMzQ1
-
-decode('Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzEyMzQ1')
-  // => { type: 'Product', id: '12345' }
+```
+## decode(base64hash)
+```javascript
+decode('Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzEyMzQ1...')
+  // => { type: 'Product', id: '12345', params: { accessToken: 'abcde123' }, raw: 'Z2lkOi8...' }
+```
+## encode(type, id[, params])
+```javascript
+encode('Product', 12345, { accessToken: 'abcde123' })
+  // => Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzEyMzQ1...
 ```
 
 ## License
--- index.js
+++ index.js
@@ -7,14 +7,35 @@ export function decode (str) {
 
   const [ type, id ] = raw.split('/')
 
+  const params = (id.split('?').slice(1)[0] || '').split('&').reduce((p, q) => {
+    const [ key, value ] = q.split('=')
+    p[key] = value
+    return p
+  }, {})
+
   return {
     type,
-    id
+    id: id.split('?')[0],
+    params,
+    raw: str
   }
 }
 
-export function encode (type, id) {
-  const full = `gid://shopify/${type}/${id}`
+export function encode (type, id, params = {}) {
+  let full = `gid://shopify/${type}/${id}`
+
+  let query = []
+  const keys = Object.keys(params)
+
+  if (keys.length > 0) {
+    for (let i = 0; i < keys.length; i++) {
+      query.push(keys[i] + '=' + params[keys[i]])
+    }
+
+    query = '?' + query.join('&')
+
+    full += query
+  }
 
   return typeof window === 'undefined' ? (
     Buffer.from(full, 'utf-8').toString('base64')
